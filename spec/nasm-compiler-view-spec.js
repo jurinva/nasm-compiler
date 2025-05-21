4d231a1bb604f9fcb1bee06549ef661a780209b1
Vladimir Yurin <vladimir.yurin@stoloto.ru>
2025-05-22 00:45:34 +0300

    package generator

---
+++ spec/nasm-compiler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NasmCompilerView from '../lib/nasm-compiler-view';
+
+describe('NasmCompilerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
